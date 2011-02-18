569fc18fcd87fbb41509b3d06beabef0be98d7b3
John Wang <john.wang@gmail.com>
2011-02-18 09:17:21 -0800

    clean up

--- src/main/scala/com/linkedin/chirper/streamer/ChirperStreamProcessor.scala
+++ src/main/scala/com/linkedin/chirper/streamer/ChirperStreamProcessor.scala
@@ -10,22 +10,39 @@ import org.json._
 import com.linkedin.led.twitter.Config
 import com.linkedin.led.twitter.streaming.StreamProcessor
 
+
+import kafka.message._
+import kafka.producer._
+
+import voldemort.scalmert.client.StoreClient
+import voldemort.client.ClientConfig
+import voldemort.client.SocketStoreClientFactory
+import voldemort.scalmert.Implicits._
+import voldemort.scalmert.versioning._
+
 class ChirperStreamProcessor extends StreamProcessor{
 	val voldurl = Config.readString("voldemort.url")
 	val kafkaHost = Config.readString("kafka.host")
 	val kafkaPort = Config.readInt("kafka.port")
 	val kafkaTopic = Config.readString("kafka.topic")
+	val voldemortUrl = Config.readString("voldemort.url")
+	val voldemortStore = Config.readString("voldemort.store")
+	
+	val factory = new SocketStoreClientFactory(new ClientConfig().setBootstrapUrls(voldemortUrl));
+	val tweetStore: StoreClient[String, String] = factory.getStoreClient[String, String](voldemortStore)
+	val kafkaProducer = new SimpleProducer(kafkaHost,kafkaPort, 64 * 1024, 100000, 10000)	
 	
 	override def process(is: InputStream): Unit = {
 	  val reader: BufferedReader = new BufferedReader(new InputStreamReader(is, "UTF-8"))
-
 	  var line = reader.readLine()
 	  while (line != null) {
 		val jsonObj = new JSONObject(line)
-	    println(line)
+		val id = jsonObj.getString("idStr")
+		tweetStore(id) = line
+	    val text = jsonObj.getString("text")
+	    kafkaProducer.send(kafkaTopic,new ByteBufferMessageSet(new Message(text.getBytes("UTF8"))))
 	    line = reader.readLine()
 	  }
-
 	  is.close
 	}
 }
