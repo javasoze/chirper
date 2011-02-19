e84e4901a3d630b8226d80b2cb9d710a2d6c037d
Alejandro Crosa <acrosa@aac-mn.local>
2011-02-18 18:52:09 -0800

    slower animation

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,5 @@
 var clouds = setInterval(function() {
   var div = $("#chirper-search-app .header").css("background-position-x");
   var x_position = div.substring(0, div.length - 2);
-  $("#chirper-search-app .header").css("background-position-x", (parseInt(x_position, 10) + 100) + "px");
+  $("#chirper-search-app .header").css("background-position-x", (parseInt(x_position, 10) + 50) + "px");
 }, 10000);
