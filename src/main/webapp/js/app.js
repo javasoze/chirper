5131e5414122b7c05a2f8a01aa97c353b30849d6
Alejandro Crosa <acrosa@aac-mn.local>
2011-02-19 12:53:59 -0800

    UI work in progress

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,19 @@
-var clouds = setInterval(function() {
-  var div = $("#chirper-search-app .header").css("background-position-x");
-  var x_position = div.substring(0, div.length - 2);
-  $("#chirper-search-app .header").css("background-position-x", (parseInt(x_position, 10) + 50) + "px");
-}, 10000);
+var Chirper = {
+
+  cloudsInterval: undefined,
+
+  init: function() {
+    this.cloudsInterval = setInterval(this.moveClouds, 10000);
+    this.moveClouds();
+  },
+
+  moveClouds: function() {
+    var div = $("#chirper-search-app .header").css("background-position-x");
+    var x_position = div.substring(0, div.length - 2);
+    $("#chirper-search-app .header").css("background-position-x", (parseInt(x_position, 10) + 50) + "px");
+  }
+};
+
+$(document).ready(function() {
+  Chirper.init();
+});
