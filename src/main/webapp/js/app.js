ee48486c95c675295b569f6bc689d8eba253f6dc
Alejandro Crosa <acrosa@aac-mn.local>
2011-02-19 18:07:08 -0800

    ui wip

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,8 +1,84 @@
+$(function(){
+  window.Tweet = Backbone.Model.extend({
+  });
+
+  var TweetList = Backbone.Collection.extend({
+    model: Tweet,
+
+    url: "/search",
+
+    parse: function(resp) {
+      this.total = resp.totaldocs;
+      this.query = resp.parsedquery;
+      this.models = resp.hits;
+      return resp.hits;
+    },
+  });
+
+  window.Tweets = new TweetList();
+
+  window.TweetView = Backbone.View.extend({
+
+    tag: "li",
+
+    initialize: function() {
+      // _.bindAll(this, 'render', 'close');
+      this.model.bind('change', this.render);
+      this.model.view = this;
+    },
+
+    render: function() {
+      $(this.el).html(Mustache.to_html($('#tweet-view').html(), this.model.toJSON()));
+      return this;
+    }
+  });
+
+  window.AppView = Backbone.View.extend({
+    el: $("#chirper-search-app"),
+
+    events: {
+      "keyup #q":  "search",
+      "click button": "search"
+    },
+
+    initialize: function() {
+      _.bindAll(this, 'addOne', 'addAll', 'render');
+
+      Tweets.bind('refresh', this.addAll);
+      Tweets.bind('all',     this.render);
+
+      Tweets.fetch();
+    },
+
+    render: function() {
+      $("#search-form").submit(function() { return false; }); // Don't submit form, use live search
+    },
+
+    addAll: function() {
+      this.$("#tweets").html("");
+      Tweets.each(this.addTweet);
+    },
+
+    addTweet: function(tweet) {
+      var view = new TweetView({ model: tweet });
+      this.$("#tweets").append(view.render().el);
+    },
+
+    search: function() {
+      Tweets.url = "/search?q="+ this.$("#q").val();
+      Tweets.fetch();
+    }
+  });
+
+  window.App = new AppView;
+});
+
 var Chirper = {
 
   cloudsInterval: undefined,
 
   init: function() {
+    // this.addSearchListeners();
     this.cloudsInterval = setInterval(this.moveClouds, 10000);
     this.moveClouds();
   },
@@ -12,6 +88,19 @@ var Chirper = {
     var x_position = div.substring(0, div.length - 2);
     $("#chirper-search-app .header").css("background-position-x", (parseInt(x_position, 10) + 50) + "px");
   }
+  // ,
+  // 
+  // addSearchListeners: function() {
+  //   var that = this;
+  //   $("#search-form").submit(function() {
+  //     that.search($("#q").val());
+  //     return false;
+  //   });
+  // },
+  // 
+  // search: function(query) {
+  //   $.ajax
+  // }
 };
 
 $(document).ready(function() {
